885d98e2ec5e181f323445891cb52f9ef06b2e03
Julio <juliointelek@hotmail.com>
2018-06-14 00:48:58 +0200

    27 casos posibles simplificados a 21 sin tener en cuenta hidratos

--- Diabetes.java
+++ Diabetes.java
@@ -8,37 +8,97 @@ public class Diabetes {
 	int SUPER_HIPER = 250;
 	
 	double ratioInsulina = 0;
-	double dosisDiariaTotal = 0;
+	double ratioInsulinaPorHidratos = 0;
 	
 	
-	public double CalcularDosis(double dosisAnterior, double glucosaPrevia, double glucosaPosterior, double glucosaActual){
+	public double CalcularDosis(double insulinaAyer, double hidratosAyer, double glucosaPreviaAyer, double glucosaPosteriorAyer, double totalInsulinaAyer, double glucosaActual, double hidratosActual){
 		
-		double dosisProvisional = 0;
+		//Formulas basicas
+		ratioInsulina = 1800 / totalInsulinaAyer;
+		ratioInsulinaPorHidratos = insulinaAyer / hidratosAyer;
 		
-		dosisProvisional= ComprobarGlucosaDiaAnterior(dosisAnterior, glucosaPrevia, glucosaPosterior);
+		int accion = 0;
+				
+		//posibles 27 casos
 		
-		
-
-		return 0;
-	}
-	
-	public double CalcularRatioInsulina (double dosisTotal)
-	{
-		return (1800/dosisDiariaTotal);
-	}
-	
-	public double ComprobarGlucosaDiaAnterior(double dosisAnterior, double glucosaPrevia, double glucosaPosterior) {
-		
-		double dosisProvisional = 0;
-		// Si la glucosa del dia previo fue correcta. la dosis adecuada es la misma.
-		if (glucosaPrevia >=70 && glucosaPrevia <= 180) {
-			dosisProvisional = dosisAnterior;
+		//Primeros 8 casos cuando la glucosa previa de ayer era alta
+		if(glucosaPreviaAyer > 140)
+		{
+			//Si la glucosa posterior tambien fue alta
+			if(glucosaPosteriorAyer >180)
+			{
+				//Si la glucosa actual es alta
+				if(glucosaActual>140){accion=1;}
+				//Si la glucosa actual es baja
+				else if(glucosaActual<80){accion=(-1);}
+			}
+			//Si la glucosa posterior fue correcta
+			else if(glucosaPosteriorAyer<180 && glucosaPosteriorAyer>80)
+				{
+					//Si la glucosa actual no es alta
+					if(glucosaActual<180){accion=(-1);}
+				}
+			//Si la glucosa posterior fue baja
+			else if(glucosaPosteriorAyer<80)
+			{
+				//da igual como este la actual... hay que disminuir la dosis
+				
+			}
 		}
+		//7 casos cuando la glucosa previa de ayer era correcta
+		else if (glucosaPreviaAyer <140 && glucosaPreviaAyer>80)
+			{
+			//si la glucosa posterior fue alta
+			if(glucosaPosteriorAyer >180)
+				{
+				//si la glucosa actual no es baja
+				if(glucosaActual>80){accion=1;}
+				}
+			//si la glucosa posterior fue correcta
+			else if(glucosaPosteriorAyer<140 && glucosaPosteriorAyer>80)
+			{
+				//Si la glucosa actual es alta
+				if(glucosaActual>140){accion=1;}
+				//Si la glucosa actual es baja
+				else if(glucosaActual<80){accion=(-1);}
+			}
+			//si la glucosa posterior fue baja
+			else if (glucosaPosteriorAyer<80)
+			{
+				//Si la glucosa actual no es alta
+				if(glucosaActual<180){accion=(-1);}
+			}	
+		}
+		//ultimos 6 casos cuando la glucosa previa de ayer era baja
+		else if(glucosaPreviaAyer<80)
+			{
+			//Cuando la glucosa posterior ayer era alta
+				if(glucosaPosteriorAyer>180)
+				{
+					//en cualquier caso aumentamos la dosis
+					accion=1;
+				}
+			//Cuando la glucosa posterior de ayer era correcta
+				else if (glucosaPosteriorAyer<180 && glucosaPosteriorAyer>80)
+				{
+					//si la glucosa actual es mayor de 80 hay que aumentar la dosis
+					if(glucosaActual>80){accion=1;}
+				}
+			//Cuando la glucosa posterior de ayer era baja
+				else if (glucosaPosteriorAyer<80)
+				{
+					//Si la glucosa actual es alta
+					if(glucosaActual>140){accion=1;}
+					//Si la glucosa actual es baja
+					else if(glucosaActual<80){accion=(-1);}
+				}
+			
+			}
 		
-		
-		
-		return dosisProvisional;
+		return accion;
 	}
+	
+
 	
 
 	public static void main(String[] args) {
