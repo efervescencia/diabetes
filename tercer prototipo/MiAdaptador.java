a14f9308a3e71d74e6c11d17e80ab85c84623e7b
Julio <juliointelek@hotmail.com>
2018-12-01 20:34:42 +0100

    Modificado el adapter del list para indicar hipos e hipers
    Cuando hay situacion de hipoglucemia o hiperglucemia, el adapter
    del list modifica el drawable de textview en cuestion para
    indicar peligro en la lectura.

--- tercer prototipo/MiAdaptador.java
+++ tercer prototipo/MiAdaptador.java
@@ -1,13 +1,17 @@
 package efervescencia.es.myapplication;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Vector;
+
 
 public class MiAdaptador extends BaseAdapter {
 
@@ -45,8 +49,40 @@ public class MiAdaptador extends BaseAdapter {
         TextView tvIngesta = (TextView) view.findViewById(R.id.listadoIngesta);
         tvIngesta.setText((CharSequence) lecturas.get(position).getIngesta());
         TextView tvGlucosaPrevia = (TextView) view.findViewById(R.id.listadoGlucosaPrevia);
+
+        //DETECTAMOS VALORES PELIGROSOS Y ACTIVAMOS AVISO
+        int gp = 0;
+        if(lecturas.get(position).getGlucosaPrevia().length()>0){
+            gp = Integer.parseInt(lecturas.get(position).getGlucosaPrevia());
+            if(gp>260 || gp<80){
+
+                Drawable image = actividad.getResources().getDrawable(R.drawable.ic_warning_black_24dp );
+                int h = image.getIntrinsicHeight();
+                int w = image.getIntrinsicWidth();
+                image.setBounds( 0, 0, w, h );
+                tvGlucosaPrevia.setCompoundDrawables( image, null, null, null );
+
+            }
+        }
+
         tvGlucosaPrevia.setText((CharSequence) lecturas.get(position).getGlucosaPrevia());
+
         TextView tvGlucosaPosterior = (TextView) view.findViewById(R.id.listadoGlucosaPosterior);
+
+        //DETECTAMOS VALORES PELIGROSOS Y ACTIVAMOS AVISO
+        int gpost = 0;
+        if(lecturas.get(position).getGlucosaPosterior().length()>0){
+            gpost = Integer.parseInt(lecturas.get(position).getGlucosaPosterior());
+            if(gpost>260 || gpost<80){
+
+                Drawable image = actividad.getResources().getDrawable( R.drawable.ic_warning_black_24dp );
+                int h = image.getIntrinsicHeight();
+                int w = image.getIntrinsicWidth();
+                image.setBounds( 0, 0, w, h );
+                tvGlucosaPosterior.setCompoundDrawables( image, null, null, null );
+            }
+        }
+
         tvGlucosaPosterior.setText((CharSequence) lecturas.get(position).getGlucosaPosterior());
         return view;
     }
