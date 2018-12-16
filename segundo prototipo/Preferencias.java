dc3a881edccb4148398d63a9129b6d227cd68c3c
Julio <juliointelek@hotmail.com>
2018-12-16 23:15:21 +0100

    Iniciado sistema para comprobar que los settings están en rango
    Implementado el listener para detectar los cambios en la configuración
    de la app, y poder comprobar que los valores introducidos son correctos
    y no pueden llevar a un mal funcionamiento del algoritmo de
    dosificación de insulina.

--- segundo prototipo/Preferencias.java
+++ segundo prototipo/Preferencias.java
@@ -1,17 +1,24 @@
 package efervescencia.es.myapplication;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 
-public class Preferencias extends PreferenceActivity {
+public class Preferencias extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    Object listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
+
+
 
     public static class MyPreferenceFragment extends PreferenceFragment
     {
@@ -23,6 +30,42 @@ public class Preferencias extends PreferenceActivity {
         }
     }
 
-}
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
 
+
+        String cadena = sharedPreferences.getString(s,"");
+        int number=0;
+        Toast toast = Toast.makeText(getBaseContext(), "Valor no permitido", Toast.LENGTH_LONG);
+
+        if(cadena.length()>0){
+            number = Integer.parseInt(cadena);
+        }
+        switch(s){
+            case "hipo": if(number<60 || number > 90){
+
+                toast.show();
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+
+
+}
 
